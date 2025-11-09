ebfbde1a3749cbbf5517cefea544cfdb3a7a63ce
aeneasr <3372410+aeneasr@users.noreply.github.com>
2025-11-09 17:09:59 +0000

    autogen: regenerate OpenAPI client for v25.4.0
    Version: v25.4.0

--- lib/src/api/metadata_api.dart
+++ lib/src/api/metadata_api.dart
@@ -8,22 +8,20 @@ import 'package:built_value/json_object.dart';
 import 'package:built_value/serializer.dart';
 import 'package:dio/dio.dart';
 
-import 'package:built_collection/built_collection.dart';
-import 'package:ory_oathkeeper_client/src/api_util.dart';
-import 'package:ory_oathkeeper_client/src/model/generic_error.dart';
-import 'package:ory_oathkeeper_client/src/model/json_web_key_set.dart';
-import 'package:ory_oathkeeper_client/src/model/rule.dart';
+import 'package:ory_oathkeeper_client/src/model/get_version200_response.dart';
+import 'package:ory_oathkeeper_client/src/model/is_alive200_response.dart';
+import 'package:ory_oathkeeper_client/src/model/is_ready503_response.dart';
 
-class ApiApi {
+class MetadataApi {
 
   final Dio _dio;
 
   final Serializers _serializers;
 
-  const ApiApi(this._dio, this._serializers);
+  const MetadataApi(this._dio, this._serializers);
 
-  /// Access Control Decision API
-  /// &gt; This endpoint works with all HTTP Methods (GET, POST, PUT, ...) and matches every path prefixed with /decisions.  This endpoint mirrors the proxy capability of ORY Oathkeeper&#39;s proxy functionality but instead of forwarding the request to the upstream server, returns 200 (request should be allowed), 401 (unauthorized), or 403 (forbidden) status codes. This endpoint can be used to integrate with other API Proxies like Ambassador, Kong, Envoy, and many more.
+  /// Return Running Software Version.
+  /// This endpoint returns the version of Ory Oathkeeper.  If the service supports TLS Edge Termination, this endpoint does not require the &#x60;X-Forwarded-Proto&#x60; header to be set.  Be aware that if you are running multiple nodes of this service, the version will never refer to the cluster state, only to a single instance.
   ///
   /// Parameters:
   /// * [cancelToken] - A [CancelToken] that can be used to cancel the operation
@@ -33,9 +31,9 @@ class ApiApi {
   /// * [onSendProgress] - A [ProgressCallback] that can be used to get the send progress
   /// * [onReceiveProgress] - A [ProgressCallback] that can be used to get the receive progress
   ///
-  /// Returns a [Future]
+  /// Returns a [Future] containing a [Response] with a [GetVersion200Response] as data
   /// Throws [DioException] if API call or serialization fails
-  Future<Response<void>> decisions({ 
+  Future<Response<GetVersion200Response>> getVersion({ 
     CancelToken? cancelToken,
     Map<String, dynamic>? headers,
     Map<String, dynamic>? extra,
@@ -43,7 +41,7 @@ class ApiApi {
     ProgressCallback? onSendProgress,
     ProgressCallback? onReceiveProgress,
   }) async {
-    final _path = r'/decisions';
+    final _path = r'/version';
     final _options = Options(
       method: r'GET',
       headers: <String, dynamic>{
@@ -64,61 +62,14 @@ class ApiApi {
       onReceiveProgress: onReceiveProgress,
     );
 
-    return _response;
-  }
-
-  /// Retrieve a Rule
-  /// Use this method to retrieve a rule from the storage. If it does not exist you will receive a 404 error.
-  ///
-  /// Parameters:
-  /// * [id] 
-  /// * [cancelToken] - A [CancelToken] that can be used to cancel the operation
-  /// * [headers] - Can be used to add additional headers to the request
-  /// * [extras] - Can be used to add flags to the request
-  /// * [validateStatus] - A [ValidateStatus] callback that can be used to determine request success based on the HTTP status of the response
-  /// * [onSendProgress] - A [ProgressCallback] that can be used to get the send progress
-  /// * [onReceiveProgress] - A [ProgressCallback] that can be used to get the receive progress
-  ///
-  /// Returns a [Future] containing a [Response] with a [Rule] as data
-  /// Throws [DioException] if API call or serialization fails
-  Future<Response<Rule>> getRule({ 
-    required String id,
-    CancelToken? cancelToken,
-    Map<String, dynamic>? headers,
-    Map<String, dynamic>? extra,
-    ValidateStatus? validateStatus,
-    ProgressCallback? onSendProgress,
-    ProgressCallback? onReceiveProgress,
-  }) async {
-    final _path = r'/rules/{id}'.replaceAll('{' r'id' '}', encodeQueryParameter(_serializers, id, const FullType(String)).toString());
-    final _options = Options(
-      method: r'GET',
-      headers: <String, dynamic>{
-        ...?headers,
-      },
-      extra: <String, dynamic>{
-        'secure': <Map<String, String>>[],
-        ...?extra,
-      },
-      validateStatus: validateStatus,
-    );
-
-    final _response = await _dio.request<Object>(
-      _path,
-      options: _options,
-      cancelToken: cancelToken,
-      onSendProgress: onSendProgress,
-      onReceiveProgress: onReceiveProgress,
-    );
-
-    Rule? _responseData;
+    GetVersion200Response? _responseData;
 
     try {
       final rawResponse = _response.data;
       _responseData = rawResponse == null ? null : _serializers.deserialize(
         rawResponse,
-        specifiedType: const FullType(Rule),
-      ) as Rule;
+        specifiedType: const FullType(GetVersion200Response),
+      ) as GetVersion200Response;
 
     } catch (error, stackTrace) {
       throw DioException(
@@ -130,7 +81,7 @@ class ApiApi {
       );
     }
 
-    return Response<Rule>(
+    return Response<GetVersion200Response>(
       data: _responseData,
       headers: _response.headers,
       isRedirect: _response.isRedirect,
@@ -142,8 +93,8 @@ class ApiApi {
     );
   }
 
-  /// Lists Cryptographic Keys
-  /// This endpoint returns cryptographic keys that are required to, for example, verify signatures of ID Tokens.
+  /// Check HTTP Server Status
+  /// This endpoint returns a HTTP 200 status code when Ory Oathkeeper is accepting incoming HTTP requests. This status does currently not include checks whether the database connection is working.  If the service supports TLS Edge Termination, this endpoint does not require the &#x60;X-Forwarded-Proto&#x60; header to be set.  Be aware that if you are running multiple nodes of this service, the health status will never refer to the cluster state, only to a single instance.
   ///
   /// Parameters:
   /// * [cancelToken] - A [CancelToken] that can be used to cancel the operation
@@ -153,9 +104,9 @@ class ApiApi {
   /// * [onSendProgress] - A [ProgressCallback] that can be used to get the send progress
   /// * [onReceiveProgress] - A [ProgressCallback] that can be used to get the receive progress
   ///
-  /// Returns a [Future] containing a [Response] with a [JsonWebKeySet] as data
+  /// Returns a [Future] containing a [Response] with a [IsAlive200Response] as data
   /// Throws [DioException] if API call or serialization fails
-  Future<Response<JsonWebKeySet>> getWellKnownJSONWebKeys({ 
+  Future<Response<IsAlive200Response>> isAlive({ 
     CancelToken? cancelToken,
     Map<String, dynamic>? headers,
     Map<String, dynamic>? extra,
@@ -163,7 +114,7 @@ class ApiApi {
     ProgressCallback? onSendProgress,
     ProgressCallback? onReceiveProgress,
   }) async {
-    final _path = r'/.well-known/jwks.json';
+    final _path = r'/health/alive';
     final _options = Options(
       method: r'GET',
       headers: <String, dynamic>{
@@ -184,14 +135,14 @@ class ApiApi {
       onReceiveProgress: onReceiveProgress,
     );
 
-    JsonWebKeySet? _responseData;
+    IsAlive200Response? _responseData;
 
     try {
       final rawResponse = _response.data;
       _responseData = rawResponse == null ? null : _serializers.deserialize(
         rawResponse,
-        specifiedType: const FullType(JsonWebKeySet),
-      ) as JsonWebKeySet;
+        specifiedType: const FullType(IsAlive200Response),
+      ) as IsAlive200Response;
 
     } catch (error, stackTrace) {
       throw DioException(
@@ -203,7 +154,7 @@ class ApiApi {
       );
     }
 
-    return Response<JsonWebKeySet>(
+    return Response<IsAlive200Response>(
       data: _responseData,
       headers: _response.headers,
       isRedirect: _response.isRedirect,
@@ -215,12 +166,10 @@ class ApiApi {
     );
   }
 
-  /// List All Rules
-  /// This method returns an array of all rules that are stored in the backend. This is useful if you want to get a full view of what rules you have currently in place.
+  /// Check HTTP Server and Database Status
+  /// This endpoint returns a HTTP 200 status code when Ory Oathkeeper is up running and the environment dependencies (e.g. the database) are responsive as well.  If the service supports TLS Edge Termination, this endpoint does not require the &#x60;X-Forwarded-Proto&#x60; header to be set.  Be aware that if you are running multiple nodes of Ory Oathkeeper, the health status will never refer to the cluster state, only to a single instance.
   ///
   /// Parameters:
-  /// * [limit] - The maximum amount of rules returned.
-  /// * [offset] - The offset from where to start looking.
   /// * [cancelToken] - A [CancelToken] that can be used to cancel the operation
   /// * [headers] - Can be used to add additional headers to the request
   /// * [extras] - Can be used to add flags to the request
@@ -228,11 +177,9 @@ class ApiApi {
   /// * [onSendProgress] - A [ProgressCallback] that can be used to get the send progress
   /// * [onReceiveProgress] - A [ProgressCallback] that can be used to get the receive progress
   ///
-  /// Returns a [Future] containing a [Response] with a [BuiltList<Rule>] as data
+  /// Returns a [Future] containing a [Response] with a [IsAlive200Response] as data
   /// Throws [DioException] if API call or serialization fails
-  Future<Response<BuiltList<Rule>>> listRules({ 
-    int? limit,
-    int? offset,
+  Future<Response<IsAlive200Response>> isReady({ 
     CancelToken? cancelToken,
     Map<String, dynamic>? headers,
     Map<String, dynamic>? extra,
@@ -240,7 +187,7 @@ class ApiApi {
     ProgressCallback? onSendProgress,
     ProgressCallback? onReceiveProgress,
   }) async {
-    final _path = r'/rules';
+    final _path = r'/health/ready';
     final _options = Options(
       method: r'GET',
       headers: <String, dynamic>{
@@ -253,28 +200,22 @@ class ApiApi {
       validateStatus: validateStatus,
     );
 
-    final _queryParameters = <String, dynamic>{
-      if (limit != null) r'limit': encodeQueryParameter(_serializers, limit, const FullType(int)),
-      if (offset != null) r'offset': encodeQueryParameter(_serializers, offset, const FullType(int)),
-    };
-
     final _response = await _dio.request<Object>(
       _path,
       options: _options,
-      queryParameters: _queryParameters,
       cancelToken: cancelToken,
       onSendProgress: onSendProgress,
       onReceiveProgress: onReceiveProgress,
     );
 
-    BuiltList<Rule>? _responseData;
+    IsAlive200Response? _responseData;
 
     try {
       final rawResponse = _response.data;
       _responseData = rawResponse == null ? null : _serializers.deserialize(
         rawResponse,
-        specifiedType: const FullType(BuiltList, [FullType(Rule)]),
-      ) as BuiltList<Rule>;
+        specifiedType: const FullType(IsAlive200Response),
+      ) as IsAlive200Response;
 
     } catch (error, stackTrace) {
       throw DioException(
@@ -286,7 +227,7 @@ class ApiApi {
       );
     }
 
-    return Response<BuiltList<Rule>>(
+    return Response<IsAlive200Response>(
       data: _responseData,
       headers: _response.headers,
       isRedirect: _response.isRedirect,
